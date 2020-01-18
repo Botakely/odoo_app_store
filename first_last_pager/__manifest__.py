35eea97d2d9ca0a648e99408a95ec418b0e670b8
Botakely <ryantsoa@gmail,com>
2020-01-18 22:17:21 +0200

    add first_last_pager

---
+++ first_last_pager/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Firs/Last Pager",
+
+    'summary': """
+        This module helps you to browse first or last page of list in form by one click""",
+
+    'description': """
+        This module helps you to browse first or last page of list in form by one click
+    """,
+
+    'license': 'AGPL-3',
+    'author': "Samuel RAMAROSELY",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'qweb': [
+        "static/src/xml/base.xml",
+    ],
+}
